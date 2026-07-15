ea8e3a10b65e18d0314755bf68db62ef2fb2bbbc
Mauro <maurogabriely@gmail.com>
2026-07-15 02:27:31 -0300

    deb mayusc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors= require("cors");
-const { dbConeccion } = require('./db/DB_Config');
+const { dbConeccion } = require('./db/db_config');
 const app= express();
 
 
